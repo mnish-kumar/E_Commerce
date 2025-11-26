a7eb41577cdae2b472dfb66404d5ddd54b34bf12
Manish Kumar <manishkr.smi@gmail.com>
2025-11-26 15:55:41 +0530

    Add backend public folder for json-server

--- frontend/src/api/Axiosconfig.jsx
+++ frontend/src/api/Axiosconfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/",
+  baseURL: "https://e-commerce-backend-upui.onrender.com",
 });
 
 export default instance;
